60560b12630a23a3dcdb97559aa2e249b50caa9e
George K. Thiruvathukal <gkt@cs.luc.edu>
2021-05-09 23:27:36 -0500

    add bb-book.sh with proper separation when generating headings

--- works/bb-book.sh
+++ works/bb-book.sh
@@ -1,86 +1,99 @@
 #!/bin/bash
 
-if [ -f ~/zenv/bin/activate ]; then
-  # This activates a user's zenv, if present
-  source ~/zenv/bin/activate
-fi
-
-if [ ! -f mlb.db ]; then
-  # This creates mlb.db so the zfind's can do their thing
-  zimport --create --dir ../zettels/baseball-md --fullpath --database mlb.db
-fi
-
-COMMON="--database mlb.db --show-document" # establishes database & what you want to show (modifiable or optional)
+function emit_atx () {
+  echo
+  echo "${ATX} $1"
+  echo
+}
 
 # MLB Encyclopedia
 
-echo "# MLB Encyclopedia"  # The above comment is showing in Voom, this echo shows in the output doc
+ATX="#" 
+emit_atx "MLB Encyclopedia"
 
 ## The National League
 
-echo "## The National League"
+ATX="##" 
+emit_atx "The National League"
 
 ### Opening
 
- # This idiom is a here document. This string, DBD, can be anything as long as it appears before/after. 
- # Comment is 1-char indented so Voom markdown safely ignores it. Quit ok since the intent of this script
- # is to create a book outline!
-
-cat << DBD
-<!--
-This could be a comment about something we need "todo"
--->
-DBD
-
-zfind $COMMON --query-string 'tags:MLB & tags:NL'
- # Here I am using zfind to find zettels matching MLB and NL tags.
- # The --database mlb.db is implied by $COMMON as is the --show-document option (to display the document)
+ATX="###" 
+emit_atx "Opening"
+zfind $COMMON --query-string 'tags:MLB & tags:NL & tags:Opening' \
+ | pandoc -f markdown -t markdown --shift-heading-level-by=${#ATX}
 
 ### Central
 
-echo "### Central"
-zfind $COMMON --query-string 'tags:MLB & tags:"NL Central"'
- # You could also avoid $COMMON and write --database mlb.db --show-document
- # (or --show-comment/--show-note/--show-cite)
+ATX="###" 
+emit_atx "Central"
+zfind $COMMON --query-string 'tags:MLB & tags:"NL Central"' \
+ | pandoc -f markdown -t markdown --shift-heading-level-by=${#ATX}
 
 ### West
 
-echo "### West"
-zfind $COMMON --query-string 'tags:MLB & tags:"NL West"'
+ATX="###" 
+emit_atx "West"
+zfind $COMMON --query-string 'tags:MLB & tags:"NL West"' \
+ | pandoc -f markdown -t markdown --shift-heading-level-by=${#ATX}
 
 ### East
 
-echo "### East"
-zfind $COMMON --query-string 'tags:MLB & tags:"NL East"'
+ATX="###" 
+emit_atx "East"
+zfind $COMMON --query-string 'tags:MLB & tags:"NL East"' \
+ | pandoc -f markdown -t markdown --shift-heading-level-by=${#ATX}
 
 ## The American League
 
-echo "## The American League"
-zfind $COMMON --query-string 'tags:MLB & tags:AL'
+ATX="##" 
+emit_atx "The American League"
 
-### East
+### Opening
 
-echo "### East"
-zfind $COMMON --query-string 'tags:MLB & tags:"AL East"'
+ATX="###" 
+emit_atx "Opening"
+zfind $COMMON --query-string 'tags:MLB & tags:AL & tags:Opening' \
+ | pandoc -f markdown -t markdown --shift-heading-level-by=${#ATX}
 
 ### Central
 
-echo "### Central"
-zfind $COMMON --query-string 'tags:MLB & tags:"AL Central"'
+ATX="###" 
+emit_atx "Central"
+zfind $COMMON --query-string 'tags:MLB & tags:"AL Central"' \
+ | pandoc -f markdown -t markdown --shift-heading-level-by=${#ATX}
 
 ### West
 
-echo "### West"
-zfind $COMMON --query-string 'tags:MLB & tags:"AL West"'
+ATX="###" 
+emit_atx "West"
+zfind $COMMON --query-string 'tags:MLB & tags:"AL West"' \
+ | pandoc -f markdown -t markdown --shift-heading-level-by=${#ATX}
+
+### East
+
+ATX="###" 
+emit_atx "East"
+zfind $COMMON --query-string 'tags:MLB & tags:"AL East"' \
+ | pandoc -f markdown -t markdown --shift-heading-level-by=${#ATX}
 
 ## World Series Winners
 
-echo "## World Series Winners"
-zfind $COMMON --query-string 'tags:MLB & document:"World Series"'
+ATX="##" 
+emit_atx "World Series Winners"
+zfind $COMMON --query-string 'tags:MLB & document:"World Series"' \
+ | pandoc -f markdown -t markdown --shift-heading-level-by=${#ATX}
 
 ## Database examples
 
+ATX="##" 
+emit_atx "Database Examples"
+
 ### zfind count in the output
 
+ATX="###" 
+emit_atx "zfind count in the output"
+
 MLB_TEAMS=$(zfind $COMMON --query-string 'tags:MLB' --count)
-echo "There are *${MLB_TEAMS}* MLB Teams"
+echo "There are ${MLB_TEAMS} MLB Teams"
+echo
