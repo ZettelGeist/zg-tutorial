e9e9461b666d8a9f5ae1a283b69e96b18c660dd1
George K. Thiruvathukal <gkt@cs.luc.edu>
2021-06-01 17:38:15 -0500

    make generated book read-only after writing it

--- a-working-example/build-the-work.sh
+++ a-working-example/build-the-work.sh
@@ -13,6 +13,7 @@ mkdir -p ${BUILD}
 zimport --create --database ${DB_PATH} --dir ${ZETTELS}  --fullpath
 
 bash ${NAME}.sh | tee ${BUILD}/${NAME}-preview.md
+chmod ugo-w ${BUILD}/${NAME}-preview.md
 
 pushd ${BUILD}
 pandoc -V geometry:margin=1in  ${NAME}-preview.md -o ${NAME}-preview.pdf
