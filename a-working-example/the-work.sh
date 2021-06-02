6cd545ddd200f09e9ef99e64033787c68418b264
George K. Thiruvathukal <gkt@cs.luc.edu>
2021-06-02 08:05:20 -0500

    use an additional tag to distinguish the new MLB zettels from the YAML ones

--- a-working-example/the-work.sh
+++ a-working-example/the-work.sh
@@ -30,22 +30,22 @@ heading
 ### Opening
 heading
 
-zfind $COMMON --query-string 'tags:MLB & tags:NL & tags:Opening' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & tags:NL & tags:Opening' $PUBLISH  | pandoc_shift_headings
 
 ### Central
 heading
 
-zfind $COMMON --query-string 'tags:MLB & tags:"NL Central"' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & tags:"NL Central"' $PUBLISH  | pandoc_shift_headings
 
 ### West
 heading
 
-zfind $COMMON --query-string 'tags:MLB & tags:"NL West"' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & tags:"NL West"' $PUBLISH  | pandoc_shift_headings
 
 ### East
 heading
 
-zfind $COMMON --query-string 'tags:MLB & tags:"NL East"' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & tags:"NL East"' $PUBLISH  | pandoc_shift_headings
 
 ## The American League
 heading
@@ -53,27 +53,27 @@ heading
 ### Opening
 heading
 
-zfind $COMMON --query-string 'tags:MLB & tags:AL & tags:Opening' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & tags:AL & tags:Opening' $PUBLISH  | pandoc_shift_headings
 
 ### Central
 heading
 
-zfind $COMMON --query-string 'tags:MLB & tags:"AL Central"' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & tags:"AL Central"' $PUBLISH  | pandoc_shift_headings
 
 ### West
 heading
 
-zfind $COMMON --query-string 'tags:MLB & tags:"AL West"' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & tags:"AL West"' $PUBLISH  | pandoc_shift_headings
 
 ### East
 heading
 
-zfind $COMMON --query-string 'tags:MLB & tags:"AL East"' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & tags:"AL East"' $PUBLISH  | pandoc_shift_headings
 
 ## World Series Winners
 heading
 
-zfind $COMMON --query-string 'tags:MLB & document:"World Series"' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & document:"World Series"' $PUBLISH  | pandoc_shift_headings
 
 ## Database examples
 heading
@@ -81,7 +81,7 @@ heading
 ### zfind count in the output
 heading
 
-MLB_TEAMS=$(zfind $COMMON --query-string 'tags:MLB' --count)
+MLB_TEAMS=$(zfind $COMMON --query-string 'tags:MD & tags:MLB' --count)
 echo "There are ${MLB_TEAMS} MLB Teams"
 echo
 
@@ -91,7 +91,7 @@ heading
 echo "This is an example of how to process the results of a zfind to generate a list..."
 echo
 
-zfind $COMMON --query-string 'tags:MLB & tags:AL' $PUBLISH_SUMMARY_ONLY | pandoc_shift_headings
+zfind $COMMON --query-string 'tags:MD & tags:MLB & tags:AL' $PUBLISH_SUMMARY_ONLY | pandoc_shift_headings
 echo
 
 ## Conclusions
